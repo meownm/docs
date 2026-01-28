a2f59252e23ee07f7de1c9865de882e762052679
meownm <101579754+meownm@users.noreply.github.com>
2026-01-28 10:39:47 +0300

    1

--- mobile_android_java/app/src/main/java/com/demo/passport/MainActivity.java
+++ mobile_android_java/app/src/main/java/com/demo/passport/MainActivity.java
@@ -84,7 +84,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
+        ImageView imageFace = findViewById(R.id.imageFace);
+        imageFace.setScaleType(ImageView.ScaleType.CENTER_CROP);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "UI attached");
 
